a6ce6253becc17d5f93a486f4ae5ec40d0c9519e
suramya-biswas-git <79524334+suramya-biswas-git@users.noreply.github.com>
2023-10-16 09:57:22 +0530

    Add files via upload

---
+++ ClassApp1.py
@@ -0,0 +1,12 @@
+import ClassDemo1
+
+emp1=ClassDemo1.Employee("Rita",45000.00)
+emp2=ClassDemo1.Employee("Rina",56000.00)
+emp3=ClassDemo1.Employee("Richa",65000.00)
+emp1.show()
+emp2.show()
+emp3.show()
+print("---------------------")
+print(emp1)
+print(emp2)
+print(emp3)
